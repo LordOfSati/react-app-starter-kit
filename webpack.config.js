c10c6766012af2922f8b1ab012b1120789648e1c
Sathish Kumar <sathishkumar3107@gmail.com>
2017-10-15 23:56:12 +0200

    Basic webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const SOURCE_PATH = path.resolve(__dirname, 'src')
+const DESTINATION_PATH = path.resolve(__dirname, 'dist')
+
+const htmlInjectConfig = new HtmlWebpackPlugin({
+	template: SOURCE_PATH + '/app/index.html',
+	filename: 'index.html',
+	inject: 'body'
+})
+
+module.exports = {
+	entry: SOURCE_PATH + '/app/app.js',
+	output: {
+		path: DESTINATION_PATH,
+		filename: 'app.bundle.js',
+		publicPath: '/'
+	},
+	module: {
+		loaders: [
+			{
+				test: /.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: [
+						'env',
+						'react'
+					]
+				}
+			}
+		]
+	},
+	plugins: [htmlInjectConfig]
+}
