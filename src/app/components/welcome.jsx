543bf3e28e630d16077c45e7335b5fd51bb90578
Sathish Kumar <sathishkumar3107@gmail.com>
2017-10-16 00:26:45 +0200

    Basic webpack configuration

--- src/app/components/welcome.jsx
+++ src/app/components/welcome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Welcome extends React.Component {
     render () {
         return (
-            <h1>Welcome to React World!!</h1>
+            <h1>Welcome to React World!!!</h1>
         )
     }
 }
